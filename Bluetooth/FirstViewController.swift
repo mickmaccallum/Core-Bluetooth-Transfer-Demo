ba7f446a3371fef7f20b93e87b10f5106cc0ed13
Mick MacCallum <micksmaccallum@gmail.com>
2014-12-20 17:32:59 -0500

    Initial Commit

---
+++ Bluetooth/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Bluetooth
+//
+//  Created by Mick on 12/20/14.
+//  Copyright (c) 2014 MacCDevTeam LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
